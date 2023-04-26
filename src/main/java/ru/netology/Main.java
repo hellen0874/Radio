f2d7cb7b2c1acf67b6145107439bff560fe26895
Elena Musinova <en.musinova@gmail.com>
2023-04-27 01:54:37 +0300

    Volume and station are ready without tests

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package ru.netology.;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,48 @@
+package ru.netology;
+
+public class Radio {
+    private int currentStationNumber;
+    private int currentVolume;
+
+    // выбор номера станции
+    public void setCurrentStationNumber(int newCurrentStationNumber) {   // Установка номера через прямое указание
+        if (newCurrentStationNumber < 0) {
+            return;
+        }
+        if (newCurrentStationNumber > 9) {              // номер станции от 0 до 9
+            return;
+        }
+        currentStationNumber = newCurrentStationNumber;
+    }
+
+    public void nextStationNumber() {
+        int target = currentStationNumber + 1;    // установка следующего номера станции с условием 9->0
+        if (currentStationNumber = 9) {
+            setCurrentStationNumber(0);
+        }
+        setCurrentStationNumber(target);
+    }
+
+    public void prevStationNumber() {
+        int target = currentStationNumber - 1;    // установка предыдущего номера станции c условием 0-> 9
+        if (currentStationNumber = 0) {
+            setCurrentStationNumber(9);
+        }
+        setCurrentStationNumber(target);
+    }
+
+    // звук
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;  // увеличение уровня громкости на +1, после 100 ничего не делать
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;  // уменьшение уровня громкости на -1, после 0 ничего не делать
+        }
+    }
+
+}
