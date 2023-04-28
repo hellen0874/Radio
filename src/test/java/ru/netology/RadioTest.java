e1b91fd21aa43354357b738ef0093afdb8765fae
Elena Musinova <en.musinova@gmail.com>
2023-04-28 22:58:20 +0300

    Radio with constructors

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -7,14 +7,23 @@ import org.junit.jupiter.api.Assertions;
 public class RadioTest {
     @Test
 // проверка установки номера станции через прямое указание
-    public void ShouldSet5() {            // установка номера станции внутри допустимого диапазона
+    public void ShouldSetInRange() {            // установка номера станции внутри допустимого диапазона (объект создается без параметров)
         Radio radio = new Radio();
         radio.setCurrentStationNumber(5);
         int actual = radio.getCurrentStationNumber();
         int expected = 5;
         Assertions.assertEquals(expected, actual);
-
     }
+
+    @Test
+    public void ShouldSetInRangeParmetrized() {            // установка номера станции внутри допустимого диапазона (объект создается с параметрами)
+        Radio radio = new Radio(30);
+        radio.setCurrentStationNumber(15);
+        int actual = radio.getCurrentStationNumber();
+        int expected = 15;
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     public void ShouldNotSetUnder0() {            // установка номера станции меньше нуля - возврат к начальному значению
         Radio radio = new Radio();
@@ -24,58 +33,85 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
 
     }
+
     @Test
-    public void ShouldNotSetUpper9() {            // установка номера станции больше 9 - возврат к начальному значению
-        Radio radio = new Radio();
-        radio.setCurrentStationNumber(10);
+    public void ShouldNotSetUpperMax() {            // установка номера станции больше max - возврат к начальному значению
+        Radio radio = new Radio(30);
+        radio.setCurrentStationNumber(31);
         int actual = radio.getCurrentStationNumber();
         int expected = 0;
         Assertions.assertEquals(expected, actual);
 
     }
+
     @Test
     // проверка переключения на +\-1 (номер станции)
     public void ShouldAddOne() {            // переключение на +1 с середины диапазона
         Radio radio = new Radio();
         radio.setCurrentStationNumber(5);
-        radio.nextStationNumber();
+        radio.next();
         int actual = radio.getCurrentStationNumber();
         int expected = 6;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldAddOneFrom9() {            // переключение на +1 с конца диапазона (граничные значения)
         Radio radio = new Radio();
         radio.setCurrentStationNumber(9);
-        radio.nextStationNumber();
+        radio.next();
         int actual = radio.getCurrentStationNumber();
         int expected = 0;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldAddOneFrom8() {            // переключение на +1 с (конца диапазона - 1) (граничные значения)
         Radio radio = new Radio();
         radio.setCurrentStationNumber(8);
-        radio.nextStationNumber();
+        radio.next();
         int actual = radio.getCurrentStationNumber();
         int expected = 9;
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void ShouldAddOneFromMax() {            // переключение на +1 с конца диапазона, объект с параметром (граничные значения)
+        Radio radio = new Radio(31);
+        radio.setCurrentStationNumber(30);
+        radio.next();
+
+        int actual = radio.getCurrentStationNumber();
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldAddOneFromPreMax() {            // переключение на +1 с (конца диапазона - 1), объект с параметром (граничные значения)
+        Radio radio = new Radio(31);
+        radio.setCurrentStationNumber(29);
+        radio.next();
+        int actual = radio.getCurrentStationNumber();
+        int expected = 30;
+        Assertions.assertEquals(expected, actual);
+    }
+
     // переключение на +1 с (конца диапазона + 1) (граничные значения) - не проверяем, это уже проверено, больше 9 номер станции не проставится
     @Test
     public void ShouldAddOneFrom0() {            // переключение на +1 с начала диапазона (граничные значения) - может, и не нужно, но на всякий случай
         Radio radio = new Radio();
         radio.setCurrentStationNumber(0);
-        radio.nextStationNumber();
+        radio.next();
         int actual = radio.getCurrentStationNumber();
         int expected = 1;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldAddOneFrom1() {            // переключение на +1 с (начала диапазона + 1) (граничные значения) - может, и не нужно, но на всякий случай
         Radio radio = new Radio();
         radio.setCurrentStationNumber(1);
-        radio.nextStationNumber();
+        radio.next();
         int actual = radio.getCurrentStationNumber();
         int expected = 2;
         Assertions.assertEquals(expected, actual);
@@ -87,44 +123,48 @@ public class RadioTest {
     public void ShouldRemoveOne() {            // переключение на -1 с середины диапазона
         Radio radio = new Radio();
         radio.setCurrentStationNumber(5);
-        radio.prevStationNumber();
+        radio.prev();
         int actual = radio.getCurrentStationNumber();
         int expected = 4;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldRemoveOneFrom9() {            // переключение на -1 с конца диапазона (граничные значения) - может, и не надо, но на всякий случай
         Radio radio = new Radio();
         radio.setCurrentStationNumber(9);
-        radio.prevStationNumber();
+        radio.prev();
         int actual = radio.getCurrentStationNumber();
         int expected = 8;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldRemoveOneFrom8() {            // переключение на -1 с (конца диапазона - 1) (граничные значения) - может, и не надо, но на всякий случай
         Radio radio = new Radio();
         radio.setCurrentStationNumber(8);
-        radio.prevStationNumber();
+        radio.prev();
         int actual = radio.getCurrentStationNumber();
         int expected = 7;
         Assertions.assertEquals(expected, actual);
     }
+
     // переключение на -1 с (конца диапазона + 1) (граничные значения) - не проверяем, это уже проверено, больше 9 номер станции не проставится
     @Test
     public void ShouldRemoveOneFrom0() {            // переключение на -1 с начала диапазона (граничные значения)
         Radio radio = new Radio();
         radio.setCurrentStationNumber(0);
-        radio.prevStationNumber();
+        radio.prev();
         int actual = radio.getCurrentStationNumber();
         int expected = 9;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldRemoveOneFrom1() {            // переключение на -1 с (начала диапазона + 1) (граничные значения) - может, и не нужно, но на всякий случай
         Radio radio = new Radio();
         radio.setCurrentStationNumber(1);
-        radio.prevStationNumber();
+        radio.prev();
         int actual = radio.getCurrentStationNumber();
         int expected = 0;
         Assertions.assertEquals(expected, actual);
@@ -149,6 +189,7 @@ public class RadioTest {
         int expected = 0;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldAddOneToVolume() {            // переключение на +1 с середины диапазона
         Radio radio = new Radio();
@@ -168,6 +209,7 @@ public class RadioTest {
         int expected = 100;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldAddOneToVolumeFrom99() {            // переключение на +1 с (конца диапазона - 1) (граничные значения)
         Radio radio = new Radio();
@@ -177,6 +219,7 @@ public class RadioTest {
         int expected = 100;
         Assertions.assertEquals(expected, actual);
     }
+
     // переключение на +1 с (конца диапазона + 1) не проверяем, установить уровень 101 не даст
     @Test
     public void ShouldRemoveOneFromVolume5() {            // переключение на -1 с середины диапазона
@@ -187,6 +230,7 @@ public class RadioTest {
         int expected = 4;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldNotRemoveOneFromVolume0() {            // переключение на -1 с начала диапазона
         Radio radio = new Radio();
@@ -196,6 +240,7 @@ public class RadioTest {
         int expected = 0;
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldNotRemoveOneFromVolume1() {            // переключение на -1 с (начала диапазона + 1)
         Radio radio = new Radio();
@@ -206,4 +251,5 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
     // переключение на -1 с (начала диапазона - 1) не проверяем, установить уровень ниже 0 не даст
+
 }
